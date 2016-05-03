f5f4030010f565b0564ae1971cbb86db0303132f
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-05-03 12:16:41 +1200

    Add clang/lcov

--- setup/developer_tools.sh
+++ setup/developer_tools.sh
@@ -6,4 +6,4 @@
 sudo dnf groupinstall -y "C Development Tools and Libraries"
 
 # Packages for building Qubes and various other projects
-sudo dnf install -y git git-email createrepo rpm-build make wget rpmdevtools python-devel python-sh python-yaml dialog rpm-sign zlib-devel perl-devel curl-devel expat-devel gettext-devel openssl-devel
+sudo dnf install -y git git-email createrepo rpm-build make wget rpmdevtools python-devel python-sh python-yaml python-tools dialog rpm-sign zlib-devel perl-devel curl-devel expat-devel gettext-devel openssl-devel python3-devel clang clang-analyzer lcov
